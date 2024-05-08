e715e5410148e7370566005bae8a53be002d5f21
alaosama <alahyaneosama1@gmail.com>
2024-05-08 14:00:26 +0200

    Quiz App

---
+++ QuizApp/scripts/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const app = document.getElementById("quiz-app");
+const quizCard = document.getElementById("quiz-details");
+const questionsCard = document.getElementById("questions-card");
+const resultCard = document.getElementById("result-card");
