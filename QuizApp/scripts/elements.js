0b047c071a86fc0205349dbe2c84854919f255b3
alaosama <alahyaneosama1@gmail.com>
2024-05-10 13:47:03 +0200

    Quiz App

--- QuizApp/scripts/elements.js
+++ QuizApp/scripts/elements.js
@@ -1,6 +1,7 @@
 "use strict";
 
 class QuizElementsHelper {
+
   constructor(app, quizCard, questionCard, resultCard, quiz) {
     this.app = app;
     this.quiz = quiz;
@@ -17,4 +18,57 @@ class QuizElementsHelper {
     // Show quiz details card
     this.showQuizCard();
   }
+
+  assignElements() {
+    // Quiz Card Elements
+    this.quizCard.startBtn = this.quizCard.querySelector(
+      ".quiz-details__start-btn"
+    );
+    this.quizCard.titleElm = this.quizCard.querySelector(
+      ".quiz-details__title"
+    );
+    this.quizCard.descriptionElm = this.quizCard.querySelector(
+      ".quiz-details__description"
+    );
+    this.quizCard.metaQCElm = this.quizCard.querySelector(
+      ".quiz-details__meta.--qc strong"
+    );
+    this.quizCard.metaTimeElm = this.quizCard.querySelector(
+      ".quiz-details__meta.--t strong"
+    );
+
+    // Question Card Elements
+    this.questionCard.progressRemainingTimeElm = document.querySelector(
+      ".questions-card__remaining-time"
+    );
+    this.questionCard.progressQuestionCountElm = document.querySelector(
+      ".questions-card__q-count"
+    );
+    this.questionCard.progressbarElm = document.querySelector(
+      ".questions-card__progress .--value"
+    );
+    this.questionCard.questionTitleElm = document.getElementById(
+      "question-title"
+    );
+    this.questionCard.optionOneElm = document.querySelector(
+      "#option-one ~ label"
+    );
+    this.questionCard.optionTwoElm = document.querySelector(
+      "#option-two ~ label"
+    );
+    this.questionCard.optionThreeElm = document.querySelector(
+      "#option-three ~ label"
+    );
+    this.questionCard.optionFourElm = document.querySelector(
+      "#option-four ~ label"
+    );
+    this.questionCard.nextBtn = this.app.querySelector("#next-btn");
+    this.questionCard.stopBtn = this.app.querySelector("#stop-btn");
+
+    // Result Card Elements
+    this.resultCard.gotoHome = this.resultCard.querySelector("#go-to-home");
+    this.resultCard.scoreElm = this.resultCard.querySelector("#score");
+  }
+
+  
 }
