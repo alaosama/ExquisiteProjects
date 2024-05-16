84f1a1e51db3d0f4faef8b5b37c2388299787bf2
alaosama <alahyaneosama1@gmail.com>
2024-05-16 20:47:59 +0200

    Number G Game

---
+++ NGuessingGame/js.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attemptsCount = 0;
+
+const guessInput = document.getElementById("guess");
+const submitButton = document.getElementById("submit");
+const hintMessage = document.getElementById("hint");
+const attemptsMessage = document.getElementById("attempts");
+
