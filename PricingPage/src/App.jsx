9cac396dd14ca5cd31697a3ef328c1c20e7cbf9e
alaosama <alahyaneosama1@gmail.com>
2024-06-16 22:32:39 +0200

    Procing Page

--- PricingPage/src/App.jsx
+++ PricingPage/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Features from './components/Features/Features'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Pricing from './componenets/Pricing/Pricing'
@@ -9,9 +7,7 @@ import Pricing from './componenets/Pricing/Pricing'
 const App = () => {
   return (
     <>
-      <Navbar />
       <Home/>
-      <Features/>
       <About/> 
       <Pricing/>
     </>
